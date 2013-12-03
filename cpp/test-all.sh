65a1f7d2b3c5827605e1e4de5e64c3a0a7354b4a
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2013-12-03 21:23:01 +0100

    set of results from my old laptop

--- cpp/test-all.sh
+++ cpp/test-all.sh
@@ -10,7 +10,7 @@ BIN=obj/test
 
 ALL='bst earlyrotate fnv32 forest-earlyrotate-10 forest-earlyrotate-12 forest-earlyrotate-14 forest-earlyrotate-16 forest-earlyrotate-18 forest-earlyrotate-3 forest-earlyrotate-4 forest-earlyrotate-6 forest-earlyrotate-8 forest-hashed-10 forest-hashed-12 forest-hashed-14 forest-hashed-16 forest-hashed-18 forest-hashed-3 forest-hashed-4 forest-hashed-6 forest-hashed-8 map unordered_map'
 
-OUTPUT=outut.txt
+OUTPUT=output.txt
 
 rm -f $OUTPUT
 
